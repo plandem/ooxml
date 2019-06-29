134269cb58d550351ab9724944a8b78f5f87d93e
Andrey Gayvoronsky <plandem@gmail.com>
2019-06-29 14:21:35 -0400

    ...

--- drawing/vml/excel_client_data.go
+++ drawing/vml/excel_client_data.go
@@ -8,13 +8,12 @@ import (
 
 //ClientData is direct mapping of CT_ClientData
 type ClientData struct {
-	XMLName xml.Name `xml:"ClientData"`
-	ml.ReservedAttributes
+	XMLName xml.Name   `xml:"ClientData"`
+	Type    ObjectType `xml:"ObjectType,attr,omitempty"`
 	ml.ReservedElements
 }
 
 func (s *ClientData) MarshalXML(e *xml.Encoder, start xml.StartElement) error {
-	resolveAttributesName(s.ReservedAttributes)
 	resolveElementsName(s.ReservedElements)
 	return e.EncodeElement(*s, xml.StartElement{Name: ooxml.ApplyNamespacePrefix(NamespaceExcelPrefix, start.Name)})
 }
