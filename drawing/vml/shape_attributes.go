b6e55d8a83a882b5470bb8ac7c6dbbff599f8f8d
Andrey Gayvoronsky <plandem@gmail.com>
2019-07-02 23:54:47 -0400

    ...

--- drawing/vml/shape_attributes.go
+++ drawing/vml/shape_attributes.go
@@ -1,8 +1,11 @@
 package vml
 
 import (
+	"fmt"
 	"github.com/plandem/ooxml/drawing/vml/css"
 	"github.com/plandem/ooxml/ml"
+	"regexp"
+	"strconv"
 )
 
 //N.B.:
@@ -14,6 +17,7 @@ import (
 // can't capture namespaced and common attribute same time (e.g.: id and r:id), so theoretically it's possible to have collision.
 type shapeAttributes struct {
 	InsetMode    InsetModeType   `xml:"insetmode,attr,omitempty"`
+	resolvedID   int
 	Spt          int             `xml:"spt,attr,omitempty"`
 	Opacity      css.Fraction    `xml:"opacity,attr,omitempty"`
 	StrokeWeight css.Number      `xml:"strokeweight,attr,omitempty"`
@@ -27,4 +31,23 @@ type shapeAttributes struct {
 	InsetPen     ml.TriStateType `xml:"insetpen,attr,omitempty"`
 
 	ml.ReservedAttributes
+}
+
+var (
+	regexpShapeID  = regexp.MustCompile(`_x0000_s([\d]+)`)
+)
+
+//ResolvedID() returns ID as integer
+func (s shapeAttributes) ResolvedID() int {
+	if s.resolvedID == 0 {
+		if matched := regexpShapeID.FindSubmatch([]byte(s.ID)); len(matched) > 0 {
+			if id, err := strconv.Atoi(string(matched[1])); err != nil {
+				panic(fmt.Errorf("can't get ID of shape: %s", matched))
+			} else {
+				s.resolvedID = id
+			}
+		}
+	}
+
+	return s.resolvedID
 }
