9f17d501c5396cb97282123f29484761a574ab06
Andrey Gayvoronsky <plandem@gmail.com>
2019-08-16 18:35:08 -0400

    drawing prototype

--- drawing/dml/blip_fill_properties.go
+++ drawing/dml/blip_fill_properties.go
@@ -4,7 +4,10 @@
 
 package dml
 
-import "github.com/plandem/ooxml/ml"
+import (
+	"encoding/xml"
+	"github.com/plandem/ooxml/ml"
+)
 
 //BlipFillProperties is a direct mapping of XSD CT_BlipFillProperties
 type BlipFillProperties struct {
@@ -12,4 +15,16 @@ type BlipFillProperties struct {
 	Dpi             int   `xml:"dpi,attr,omitempty"`
 	RotateWithShape bool  `xml:"rotWithShape,attr,omitempty"`
 	ml.ReservedElements
+}
+
+//Go1.12 has limited support of namespace prefixes, so use special type with hardcoded prefixes for marshalling
+type blipFillProperties struct {
+	Blip            *Blip `xml:"a:blip,omitempty"`
+	Dpi             int   `xml:"dpi,attr,omitempty"`
+	RotateWithShape bool  `xml:"rotWithShape,attr,omitempty"`
+	ml.ReservedElements
+}
+
+func (t *BlipFillProperties) MarshalXML(e *xml.Encoder, start xml.StartElement) error {
+	return e.EncodeElement(blipFillProperties(*t), start)
 }
