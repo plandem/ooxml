9721c8653e0b9d59b46314f226cbc81e0a0eeb7f
Andrey Gayvoronsky <plandem@gmail.com>
2019-08-16 12:37:08 -0400

    drawing prototype

--- drawing/dml/coordinate.go
+++ drawing/dml/coordinate.go
@@ -4,8 +4,29 @@
 
 package dml
 
+import (
+	"encoding/xml"
+	"strconv"
+	"strings"
+)
+
 //Coordinate is a direct mapping of XSD ST_Coordinate
 //Office will read either a length followed by a unit or EMUs with no unit present, but will write only EMUs when no units are present.
 type Coordinate int
 
-//TODO: add unmarshaller to read units and convert to EMU
+//CoordinateWithUnit converts coordinate with units into EMU
+func CoordinateWithUnit(c string) (Coordinate, error) {
+	c = strings.ToLower(c)
+
+	//TODO: add parser of units and convert to EMU
+	//...
+
+	i, err := strconv.Atoi(c)
+	return Coordinate(i), err
+}
+
+//UnmarshalXMLAttr unmarshal text into Coordinate in EMU
+func (c *Coordinate) UnmarshalXMLAttr(attr xml.Attr) (err error) {
+	*c, err = CoordinateWithUnit(attr.Value)
+	return
+}
