2291bee680e86f5cbd8354ee4876bffc763eec26
Andrey Gayvoronsky <plandem@gmail.com>
2019-08-15 23:05:38 -0400

    drawing prototype

--- drawing/dml/common/primitives.go
+++ drawing/dml/common/primitives.go
@@ -1,5 +1,7 @@
 package common
 
+import "github.com/plandem/ooxml/ml"
+
 //Coordinate is a direct mapping of XSD ST_Coordinate
 type Coordinate string //ST_CoordinateUnqualified s:ST_UniversalMeasure
 
@@ -15,6 +17,15 @@ type PositiveSize2D struct {
 	Cy uint `xml:"cy,attr"`
 }
 
+//Transform2D is a direct mapping of XSD CT_Transform2D
+type Transform2D struct {
+	Offset         *Point2D        `xml:"off,omitempty"`
+	Extent         *PositiveSize2D `xml:"ext,omitempty"`
+	FlipHorizontal bool            `xml:"flipH,attr,omitempty"`
+	FlipVertical   bool            `xml:"flipV,attr,omitempty"`
+	Rotation       ml.PropertyInt  `xml:"rot,attr,omitempty"`
+}
+
 //func (n *Point2D) MarshalXML(e *xml.Encoder, start xml.StartElement) error {
 //	return e.EncodeElement(*n, xml.StartElement{Name: ml.ApplyNamespacePrefix(ml.NamespaceDrawingExcel, start.Name)})
 //}
@@ -22,4 +33,3 @@ type PositiveSize2D struct {
 //func (n *PositiveSize2D) MarshalXML(e *xml.Encoder, start xml.StartElement) error {
 //	return e.EncodeElement(*n, xml.StartElement{Name: ml.ApplyNamespacePrefix(ml.NamespaceDrawingExcel, start.Name)})
 //}
-
