67873e13a3ec0f53d32ca977942a8d910cac932f
Andrey Gayvoronsky <plandem@gmail.com>
2019-08-16 12:50:32 -0400

    drawing prototype

--- drawing/dml/non_visual_drawing_properties.go
+++ drawing/dml/non_visual_drawing_properties.go
@@ -8,7 +8,7 @@ import "github.com/plandem/ooxml/ml"
 
 //NonVisualDrawingProperties is a direct mapping of XSD CT_NonVisualDrawingProps
 type NonVisualDrawingProperties struct {
-	ID          string `xml:"id,attr"`
+	ID          int    `xml:"id,attr"`
 	Name        string `xml:"name,attr"`
 	Description string `xml:"descr,attr,omitempty"`
 	Title       string `xml:"title,attr,omitempty"`
